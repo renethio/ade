2be43380ff4255aa3b3d6de26530983f47aad190
renethio <ade@ademac.local>
2018-04-10 01:03:53 -0400

    password less thant 10 character now

--- modules/users/server/models/user.server.model.js
+++ modules/users/server/models/user.server.model.js
@@ -115,17 +115,17 @@ UserSchema.pre('save', function (next) {
 /**
  * Hook a pre validate method to test the local password
  */
-UserSchema.pre('validate', function (next) {
-  if (this.provider === 'local' && this.password && this.isModified('password')) {
-    var result = owasp.test(this.password);
-    if (result.errors.length) {
-      var error = result.errors.join(' ');
-      this.invalidate('password', error);
-    }
-  }
+// UserSchema.pre('validate', function (next) {
+//   if (this.provider === 'local' && this.password && this.isModified('password')) {
+//     var result = owasp.test(this.password);
+//     if (result.errors.length) {
+//       var error = result.errors.join(' ');
+//       this.invalidate('password', error);
+//     }
+//   }
 
-  next();
-});
+//   next();
+// });
 
 /**
  * Create instance method for hashing a password
